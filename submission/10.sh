115b0d45f337e729ced072895c5b07ce9876fdea
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-26 11:03:02 +0100

    fix: network flag

--- submission/10.sh
+++ submission/10.sh
@@ -2,4 +2,4 @@
 # Address: `bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy` 
 # using bitcoin-cli validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" it return a
 # Invalid Bech32 checksum
-bitcoin-cli -rpcwallet=btrustwallet validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | python3 -c "import sys,json; print(json.load(sys.stdin)['isvalid'])" | tr '[:upper:]' '[:lower:]'
+bitcoin-cli -regtest -rpcwallet=btrustwallet validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | python3 -c "import sys,json; print(json.load(sys.stdin)['isvalid'])" | tr '[:upper:]' '[:lower:]'
