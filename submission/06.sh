7d1e6817cef765f0add5642c7298f6f3d2db9c05
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-26 06:45:14 +0100

    fix wallet flag order

--- submission/06.sh
+++ submission/06.sh
@@ -2,4 +2,4 @@
 # label can be empty as we not need it we just have to specify it as label is the first argument while 
 # the address type is the second argument
 # label is just like a name for the address 
-bitcoin-cli  -regtest getnewaddress  -rpcwallet=builderswallet "my_legacy_address" "legacy"
+bitcoin-cli  -regtest -rpcwallet=builderswallet getnewaddress "my_legacy_address" "legacy"
--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,2 @@
 # Generate a native SegWit address.
-bitcoin-cli  -regtest getnewaddress  -rpcwallet=builderswallet "my_sigwit_address" "bech32"
+bitcoin-cli  -regtest -rpcwallet=builderswallet getnewaddress "my_sigwit_address" "bech32"
