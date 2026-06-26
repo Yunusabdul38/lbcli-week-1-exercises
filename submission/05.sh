a2fefa078f9f22e279c87d4da9eae4aa96e4b871
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-26 06:38:25 +0100

    mine block

--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,7 @@
 # Check the total amount in the wallet.
+# ci check require the balance to be greater than 0 to get a balnce i mine 101 block
+# reason for 101 is because mining reword only become spendable after 100 block has been mine 
+# ontop of the first mine block
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 $ADDR
 bitcoin-cli  -regtest -rpcwallet=builderswallet getbalance
