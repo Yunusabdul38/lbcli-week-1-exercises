080b90bf474363458347ef8e04dba450788ba0a5
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-26 06:15:33 +0100

    fix node network

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 # Write the bitcoin cli command to get the bitcoin node network name
-bitcoin-cli getblockchaininfo | grep -i "chain"
+bitcoin-cli -regtest getblockchaininfo | grep '"chain"' | awk -F'"' '{print $4}'
