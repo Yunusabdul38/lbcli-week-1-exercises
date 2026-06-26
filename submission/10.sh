71be41cd95a3d6356033f0fe455b9a3552e6cc5f
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-06-26 10:53:58 +0100

    fix: address validation with validateaddress cmd

--- submission/10.sh
+++ submission/10.sh
@@ -2,4 +2,4 @@
 # Address: `bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy` 
 # using bitcoin-cli validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" it return a
 # Invalid Bech32 checksum
-false
+bitcoin-cli -rpcwallet=btrustwallet validateaddress "bcrt1qckgvfee4qs6y98jrcn8qc0m6ce6sxls0vac3yy" | python3 -c "import sys,json; print(json.load(sys.stdin)['isvalid'])" | tr '[:upper:]' '[:lower:]'
